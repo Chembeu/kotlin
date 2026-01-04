653c6f24c1e2bdd89219b9c3d7639e381272280c
kavu235 <kavujulius1@gmail.com>
2026-01-04 09:29:48 +0300

    if....else

---
+++ if_else.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val number : Int
+    number  = 40
+    if (number > 50){
+        println("the number is greater than 50")
+    }
+    else{
+        println("the number is less than 50")
+    }
+}
