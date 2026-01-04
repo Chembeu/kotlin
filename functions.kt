5e478bd2b67a241a634dc7c930bbb2ab1d027fc9
kavu235 <kavujulius1@gmail.com>
2026-01-04 09:01:03 +0300

    functions

---
+++ functions.kt
@@ -0,0 +1,17 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+  getLoan()
+  addMarks(98, 60)
+    
+}
+fun getLoan(){
+    var haveLimit = false
+    println(haveLimit)
+}
+fun addMarks(math: Int, eng: Int){
+    val total: Int = math+eng
+    println(total)
+}
---
+++ vary.kt
@@ -0,0 +1,6 @@
+func main (){
+    val x: Int = 10
+    var y: Int = 20
+    val name: String = "shanga"
+    println("x: $x, y: $y, name: $name")    
+}
